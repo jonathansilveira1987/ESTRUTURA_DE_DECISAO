c4adfb09d9398a49bb860778bdfc688eb7063e43
jonathansilveira1987 <jonathan.silveira@outlook.com>
2021-06-22 00:38:10 -0300

    Exercícico 1

---
+++ exercicio1.py
@@ -0,0 +1,11 @@
+# 1.Faça um Programa que peça dois números e imprima o maior deles.
+# Desenvolvido por Jonathan Silveira - Instagram: @ jonathandev01
+
+num1 = int (input('Primeiro numero: '))
+
+num2 = int(input('Segundo numero: '))
+
+if num1 > num2:
+    print(num1)
+else:
+    print(num2)
