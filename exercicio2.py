a3c153cd72398f1381d08433143af6eb39b70170
jonathansilveira1987 <jonathan.silveira@outlook.com>
2021-06-22 22:00:55 -0300

    Estrutura de Decisão

---
+++ exercicio2.py
@@ -0,0 +1,12 @@
+# 2. Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+# Desenvolvido por Jonathan Silveira - Instagram: @ jonathandev01
+
+numero = int(input('digite um numero: ')) 
+
+if numero >= 0:
+
+  print(f'O numero {numero} é positivo'.format(numero))
+
+else:
+
+  print(f'O numero {numero} é negativo'.format(numero))
