4ce6b18c82893e1efd2eebfc9dfbaac65c5d6f85
jonathansilveira1987 <jonathan.silveira@outlook.com>
2021-06-24 23:43:25 -0300

    Exercício

--- exercicio33.py
+++ exercicio33.py
@@ -1,2 +1,11 @@
 # 33. Medir velocidade de internet.
 # Desenvolvido por Jonathan Silveira - Instagram: @ jonathandev01
+
+# J
+for row in range(7):
+    for col in range(5):
+        if (row == 6) or (col in {0, 4,}):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
---
+++ exercicio34.py
@@ -0,0 +1,7 @@
+for row in range(7):
+    for col in range(5):
+        if (row == 3) or (col in {0, 4}):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
