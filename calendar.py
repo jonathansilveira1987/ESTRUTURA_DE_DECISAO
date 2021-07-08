ed969636ae33e8d42d87eed58eed26b112cdf187
jonathansilveira1987 <jonathan.silveira@outlook.com>
2021-07-07 21:45:17 -0300

    Exercício

---
+++ calendar.py
@@ -0,0 +1,7 @@
+# Calendário.
+# Desenvolvido por Jonathan Silveira - Instagram: @ jonathandev01
+
+import calendar
+year = 2021
+month = 1
+print (calendar.month(year, month))
---
+++ exercicio6.py
@@ -0,0 +1,4 @@
+# 6. Faça um Programa que leia três números e mostre o maior deles.
+# Desenvolvido por Jonathan Silveira - Instagram: @ jonathandev01
+
+n1 = input('Digite o primeiro valor: ')
