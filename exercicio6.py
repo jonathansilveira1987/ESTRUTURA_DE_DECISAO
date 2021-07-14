12752d36ce650aa7220bb56fd9217f131a7ec1d5
jonathansilveira1987 <jonathan.silveira@outlook.com>
2021-07-14 00:12:11 -0300

    Exercício

--- exercicio6.py
+++ exercicio6.py
@@ -1,4 +1,13 @@
 # 6. Faça um Programa que leia três números e mostre o maior deles.
 # Desenvolvido por Jonathan Silveira - Instagram: @ jonathandev01
 
-n1 = input('Digite o primeiro valor: ')
+num1 = int(input("Número um: "))
+num2 = int(input("Número dois: "))
+num3 = int(input("Número três: "))
+
+if num1 > num2 and num1 > num3:
+    print("O número um é o maior: ", num1)
+elif num2 > num1 and num2 > num3:
+    print("O número dois é o maior: ", num2)
+elif num3 > num1 and num3 > num2:
+    print("O número três é o maior: ", num3)
