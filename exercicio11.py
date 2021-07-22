ec932a695830744bf336779005b322ae24152dc0
jonathansilveira1987 <jonathan.silveira@outlook.com>
2021-07-22 12:39:43 -0300

    Exercício

--- exercicio11.py
+++ exercicio11.py
@@ -1,4 +1,4 @@
-# 11. As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe contraram 
+# 11. As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe contrataram 
 # para desenvolver o programa que calculará os reajustes.
 # Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado 
 # no salário atual:
@@ -12,23 +12,12 @@
 # o novo salário, após o aumento.
 # Desenvolvido por Jonathan Silveira - Instagram: @ jonathandev01
 
-salario = (input("Digite o salário do colaborador: "))
-
-    if salario = 280:
-        else salario * 0.20
-    elif salario > 700:
-        salario * 0.15
-    elif salario > 1500:
-        salario * 0.10
-    else:
-        salario * 0.05
-
-    print('Salario original: R$ ', salario)
-    print('Percentual: ',percentual,'%')
-
-    percentual = percentual/100.0
-    aumento = percentual * salario
-    novo_salario = salario + aumento
-    
-    print('Aumento: R$ ',aumento)
-    print('Novo salário: R$ ', novo_salario)
+salario = float(input("Digite o salário do colaborador: "))
+if salario <= 280:
+    print("O valor do novo salário do colaborador com rejuste é: ", salario * 0.2 + salario)
+elif salario > 280 or salario < 700:
+    print("O valor do novo salário do colaborador com rejuste é: ", salario * 0.15 + salario)
+elif salario > 700 or salario < 1500:
+    print("O valor do novo salário do colaborador com rejuste é: ", salario * 0.10 + salario)
+elif salario > 1500:
+    print("O valor do novo salário do colaborador com rejuste é: ", salario * 0.05 + salario)
