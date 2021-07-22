5880062a3c39c10f7ed17ec4da70b940819d7faa
jonathansilveira1987 <jonathan.silveira@outlook.com>
2021-07-22 20:54:58 -0300

    Exercício

--- exercicio11.py
+++ exercicio11.py
@@ -2,22 +2,32 @@
 # para desenvolver o programa que calculará os reajustes.
 # Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado 
 # no salário atual:
-# salários até R$ 280,00 (incluindo) : aumento de 20%
-# salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
-# salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
-# salários de R$ 1500,00 em diante : aumento de 5% Após o aumento ser realizado, informe na tela:
-# o salário antes do reajuste;
-# o percentual de aumento aplicado;
-# o valor do aumento;
-# o novo salário, após o aumento.
+# Após o aumento ser realizado, informe na tela: O salário antes do reajuste, o percentual de aumento 
+# aplicado, o valor do aumento e o novo salário, após o aumento.
 # Desenvolvido por Jonathan Silveira - Instagram: @ jonathandev01
 
-salario = float(input("Digite o salário do colaborador: "))
-if salario <= 280:
-    print("O valor do novo salário do colaborador com rejuste é: ", salario * 0.2 + salario)
-elif salario > 280 or salario < 700:
-    print("O valor do novo salário do colaborador com rejuste é: ", salario * 0.15 + salario)
-elif salario > 700 or salario < 1500:
-    print("O valor do novo salário do colaborador com rejuste é: ", salario * 0.10 + salario)
+salario = float(input("Digite o valor atual do salário do colaborador: "))
+# salários até R$ 280,00 (incluindo) : aumento de 20%
+if salario <= 280:    
+    print ('O valor do salário antes do reajuste:', salario)
+    print ('O valor de percentual de aumento aplicado foi 20%')
+    print ('O valor do aumento foi de: ', salario * 0.2)
+    print ('O valor do novo salário com reajuste é: ', salario * 0.2 + salario)
+# salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
+elif salario > 280 and salario < 700:
+    print ('O valor do salário antes do reajuste:', salario)
+    print ('O valor de percentual de aumento aplicado foi 15%')
+    print ('O valor do aumento foi de: ', salario * 0.15)
+    print ('O valor do novo salário com reajuste é: ', salario * 0.15 + salario)
+# salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
+elif salario > 700 and salario < 1500:
+    print ('O valor do salário antes do reajuste:', salario)
+    print ('O valor de percentual de aumento aplicado foi 10%')
+    print ('O valor do aumento foi de: ', salario * 0.10)
+    print ('O valor do novo salário com reajuste é: ', salario * 0.10 + salario)
+# salários de R$ 1500,00 em diante : aumento de 5%
 elif salario > 1500:
-    print("O valor do novo salário do colaborador com rejuste é: ", salario * 0.05 + salario)
+    print ('O valor do salário antes do reajuste:', salario)
+    print ('O valor de percentual de aumento aplicado foi 5%')
+    print ('O valor do aumento foi de: ', salario * 0.05)
+    print ('O valor do novo salário com reajuste é: ', salario * 0.05 + salario)
